30fcebf3bcef0945daf75d5561462a79b662c32a
JaredMillsap <jmillsap23@gmail.com>
2021-11-17 13:40:41 -0600

    Exercise 1 LOOPS

---
+++ Loops Exercise 1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Loops_Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0;i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+
+        }
+
+
+    }
+}
